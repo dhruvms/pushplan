f6c81b471e5dbe88ffe7079fc7c222075f841c73
Dhruv Mauria Saxena <dhruvmsaxena@gmail.com>
2023-01-26 15:04:02 -0500

    cleanup files from simplan into pushplan

--- scripts/nb_files.py
+++ scripts/nb_files.py
@@ -114,7 +114,82 @@ def CreateNewSceneFile(filepath, new_scene_id):
 
 		return True
 
-def main():
+def Rewrite(filepath):
+	# old_scene_id = filepath.split('_')[2]
+
+	num_objs = 0
+	objs = {}
+	ooid = None
+	goal = None
+	with open(filepath, 'r') as f_in:
+		done = False
+		while not done:
+			line = f_in.readline()[:-1]
+
+			if len(line) == 0:
+				done = True
+
+			if line == 'O':
+				line = f_in.readline()[:-1]
+				num_objs = int(line)
+				for i in range(num_objs):
+					line = f_in.readline()[:-1]
+					obj = [float(val) for val in line.split(',')[:-1]]
+					# line = f_in.readline()[:-1]
+					# objs[obj[0]] += [line == 'True']
+					objs[obj[0]] = obj[1:]
+					objs[obj[0]] += [line.split(',')[-1] == 'True']
+
+				# ooid = np.setxor1d(list(objs.keys()), np.arange(1, num_objs+1).astype('float'))
+				# ooid = ooid[ooid != 999][0]
+				# objs[ooid] = objs.pop(999)
+				objs = collections.OrderedDict(sorted(objs.items()))
+
+			if line == 'G':
+				goal = f_in.readline()[:-1]
+
+			if line == 'ooi':
+				ooid = int(f_in.readline()[:-1])
+
+	newfilepath = filepath.replace('simplan', 'sbpl', 1)
+	newfilepath = newfilepath.replace('simplan', 'pushplan', 1)
+	newfilepath = newfilepath.replace('data', 'dat', 1)
+	with open(newfilepath, 'w') as f_out:
+		f_out.write('O\n')
+		f_out.write(str(num_objs) + '\n')
+		for oid in objs:
+			o = objs[oid]
+			o_str = str(int(oid)) + ','
+			o_str += ','.join([str(int(x)) for x in o[:2]]) + ','
+			o_str += ','.join([str(x) for x in o[2:-1]]) + ','
+			o_str += 'True\n' if o[-1] == 1 else 'False\n'
+			f_out.write(o_str)
+
+		if ooid is not None:
+			f_out.write('ooi\n')
+			f_out.write(str(int(ooid)) + '\n')
+
+		f_out.write('R\n')
+		f_out.write('0.0,0.0,0.0,0.0,0.0,0.0,1.0\n')
+
+		f_out.write('S\n')
+		f_out.write('8\n')
+		f_out.write('torso_lift_joint,0.1\n')
+		f_out.write('r_shoulder_pan_joint,-1.68651\n')
+		f_out.write('r_shoulder_lift_joint,-0.184315\n')
+		f_out.write('r_upper_arm_roll_joint,-1.67611\n')
+		f_out.write('r_elbow_flex_joint,-1.66215\n')
+		f_out.write('r_forearm_roll_joint,0.285096\n')
+		f_out.write('r_wrist_flex_joint,-0.658798\n')
+		f_out.write('r_wrist_roll_joint,1.41563\n')
+
+		if goal is not None:
+			f_out.write('G\n')
+			f_out.write(goal + '\n')
+
+	return True
+
+def main_nb():
 	new_scene_ids = np.arange(900000, 1000000)
 
 	i = 0
@@ -129,5 +204,14 @@ def main():
 			if (saved):
 				i += 1
 
+def main_rewrite():
+	for (dirpath, dirnames, filenames) in os.walk(DATADIR):
+		for filename in filenames:
+			if '.txt' not in filename or 'plan' not in filename or 'SCENE' not in filename:
+				continue
+
+			filepath = os.path.join(dirpath, filename)
+			Rewrite(filepath)
+
 if __name__ == '__main__':
-	main()
+	main_rewrite()
