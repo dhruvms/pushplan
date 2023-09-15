02f872388d794bcd6882f558bd8a1bcfafdf268c
Dhruv Mauria Saxena <dhruvmsaxena@gmail.com>
2023-09-14 22:03:54 -0400

    set globals for pr2

--- test/pr2.cpp
+++ test/pr2.cpp
@@ -1,6 +1,7 @@
 #include <pushplan/search/planner.hpp>
 #include <pushplan/utils/helpers.hpp>
 #include <pushplan/utils/constants.hpp>
+#include <pushplan/utils/discretisation.hpp>
 
 #include <smpl/debug/visualizer_ros.h>
 #include <ros/ros.h>
@@ -9,6 +10,46 @@
 #include <fstream>
 
 using namespace clutter;
+
+void readDiscretisationParams(const ros::NodeHandle& ph, WorldResolutionParams& disc_params)
+{
+	std::string disc_string;
+	if (!ph.getParam("objects/discretisation", disc_string)) {
+		throw std::runtime_error("Parameter 'objects/discretisation' not found in planning params");
+	}
+
+	auto disc = ParseMapFromString<double>(disc_string);
+	SetWorldResolutionParams(
+		disc["x"], disc["y"], disc["theta"],
+		disc["ox"], disc["oy"], disc_params);
+	DiscretisationManager::Initialize(disc_params);
+}
+
+void setupGlobals(const ros::NodeHandle& ph)
+{
+	ph.getParam("/fridge", FRIDGE);
+	ph.getParam("mapf/planning_time", MAPF_PLANNING_TIME);
+	ph.getParam("mapf/res", RES);
+	ph.getParam("mapf/goal_thresh", GOAL_THRESH);
+	ph.getParam("mapf/whca/window", WINDOW);
+	ph.getParam("mapf/whca/grid", GRID);
+	ph.getParam("robot/speed", R_SPEED);
+	ph.getParam("goal/save", SAVE);
+	ph.getParam("goal/cc_2d", CC_2D);
+	ph.getParam("goal/cc_3d", CC_3D);
+	ph.getParam("occupancy_grid/res", DF_RES);
+	ph.getParam("robot/pushing/samples", SAMPLES);
+
+	int llhc, hlhc, cp, algo;
+	ph.getParam("mapf/cbs/llhc", llhc);
+	ph.getParam("mapf/cbs/hlhc", hlhc);
+	ph.getParam("mapf/cbs/cp", cp);
+	ph.getParam("mapf/cbs/algo", algo);
+	LLHC = static_cast<LowLevelConflictHeuristic>(llhc);
+	HLHC = static_cast<HighLevelConflictHeuristic>(hlhc);
+	CP = static_cast<ConflictPrioritisation>(cp);
+	ALGO = static_cast<MAPFAlgo>(algo);
+}
 
 int main(int argc, char** argv)
 {
@@ -21,6 +62,11 @@ int main(int argc, char** argv)
 
 	// Let publishers set up
 	ros::Duration(1.0).sleep();
+
+	// setup globals
+	WorldResolutionParams disc_params;
+	setupGlobals(ph);
+	readDiscretisationParams(ph, disc_params);
 
 	std::string planfile(__FILE__);
 	auto found = planfile.find_last_of("/\\");
